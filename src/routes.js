7834f33d802cd1cd31b6b131dc155e82ebf66b4b
Joji Mathew <jojimathew.mec@gmail.com>
2019-09-07 23:38:09 +0530

    changed BrowserRouter to HashRouter for gh-pages

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import SupportPage from "./pages/support";
 import DashboardPage from "./pages/dashboard";
